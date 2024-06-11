5e57c97df355f54bda95d336e7f32c4b016aa15d
Michael Martins-Baptista <144259566+michaelmartinsb@users.noreply.github.com>
2024-06-11 11:32:55 -0400

    Modified to include Universal Windows Platform apps.

--- queryApplicationInventory/getInstalledAppsFromRegistry.ps1
+++ queryApplicationInventory/getInstalledAppsFromRegistry.ps1
@@ -10,13 +10,18 @@ $paths = @(
 # filter out entries without a display name (to ignore system components that are not applications),
 # select the display name and version for those applications, and then sort the list by application name.
 
-$installedApps = $paths | ForEach-Object { 
-# Retrieve all properties for items in each path
+$installedWin32Apps = $paths | ForEach-Object {
+    # Retrieve all properties for items in each path
     Get-ItemProperty $_\*
-} | Where-Object { 
-    $_.DisplayName -ne $null  # Filter to include only items with a display name
-} | Select-Object DisplayName, DisplayVersion | Sort-Object DisplayName # Select relevant properties # Sort the results by the display name of the applications
+} | Where-Object {
+    $_.DisplayName -ne $null # Filter to include only items with a display name
+} | Select-Object @{Name='Name';Expression={$_.DisplayName}}, @{Name='Version';Expression={$_.DisplayVersion}} | Sort-Object Name
+
+# Query installed UWP apps using Get-AppxPackage
+$installedUWPApps = Get-AppxPackage | Select-Object @{Name='Name';Expression={$_.Name}}, @{Name='Version';Expression={$_.Version}} | Sort-Object Name
+
+# Combine the lists of installed Win32 apps and UWP apps
+$installedApps = $installedWin32Apps + $installedUWPApps
 
 # Output the list of installed applications and their version numbers.
-
 $installedApps
