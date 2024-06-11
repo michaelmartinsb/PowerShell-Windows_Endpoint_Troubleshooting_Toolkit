cb9581378ac60c107083b50f3f13d0d4d7ce54af
Michael Martins-Baptista <144259566+michaelmartinsb@users.noreply.github.com>
2024-06-11 11:03:39 -0400

    Update getInstalledAppsFromRegistry.ps1 to modify comments

--- queryApplicationInventory/getInstalledAppsFromRegistry.ps1
+++ queryApplicationInventory/getInstalledAppsFromRegistry.ps1
@@ -11,11 +11,11 @@ $paths = @(
 # select the display name and version for those applications, and then sort the list by application name.
 
 $installedApps = $paths | ForEach-Object { 
-    Get-ItemProperty $_\*  # Retrieve all properties for items in each path
+# Retrieve all properties for items in each path
+    Get-ItemProperty $_\*
 } | Where-Object { 
     $_.DisplayName -ne $null  # Filter to include only items with a display name
-} | Select-Object DisplayName, DisplayVersion  # Select relevant properties
-  | Sort-Object DisplayName  # Sort the results by the display name of the applications
+} | Select-Object DisplayName, DisplayVersion | Sort-Object DisplayName # Select relevant properties # Sort the results by the display name of the applications
 
 # Output the list of installed applications and their version numbers.
 
